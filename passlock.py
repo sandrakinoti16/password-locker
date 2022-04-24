753a4b4ab2b580f91cb198fb2278224f65920d81
sandra kinoti <sandra.kinoti@student.moringaschool.com>
2022-04-24 16:44:11 +0300

    add generate random password

--- passlock.py
+++ passlock.py
@@ -79,3 +79,7 @@ class User:
         Method that returns all items in the credentials list
         """
         return cls.
+        def generatePassword(stringLength=8):
+        """Generate a random password string of letters and digits and special characters"""
+        password = string.ascii_uppercase + string.ascii_lowercase + string.digits + "~!@#$%^&*"
+        return ''.join(random.choice(password) for i in range(stringLength))
