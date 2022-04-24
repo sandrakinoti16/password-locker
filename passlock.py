72abef0e3e44c4cffdac68075c72f5282a73372c
sandra kinoti <sandra.kinoti@student.moringaschool.com>
2022-04-24 16:12:55 +0300

    create new instance of class user

--- passlock.py
+++ passlock.py
@@ -6,3 +6,10 @@ class User:
     Create User class that generates new instances of a user.
     """
     user_list = []
+    
+     def __init__(self, username, password):
+        """
+        method that defines the properties of a user.
+        """
+        self.username = username
+        self.password = password
