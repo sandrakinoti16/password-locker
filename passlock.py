c5470c06c84a80e2ad70ca072eb9cc5d36324e44
sandra kinoti <sandra.kinoti@student.moringaschool.com>
2022-04-24 16:33:48 +0300

    add append the method to delete credentials

--- passlock.py
+++ passlock.py
@@ -53,3 +53,8 @@ class User:
         method to store a new credential to the credentials list
         """
         Credentials.credentials_list.append(self)
+         def delete_credentials(self):
+        """
+        delete_credentials method that deletes an account credentials from the credentials_list
+        """
+        Credentials.credentials_list.remove(self)
