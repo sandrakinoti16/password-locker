32ca35cf55a829305475cc305182c9be11126176
sandra kinoti <sandra.kinoti@student.moringaschool.com>
2022-04-24 16:29:47 +0300

    add instaciate class for storing user credentials

--- passlock.py
+++ passlock.py
@@ -41,3 +41,10 @@ class User:
             if(user.username == username and user.password == password):
                     a_user == user.username
         return a_user
+        def __init__(self,account,userName, password):
+        """
+        method that defines user credentials to be stored
+        """
+        self.account = account
+        self.userName = userName
+        self.password = 
