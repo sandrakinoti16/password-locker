345c9626f0df6994498500305cd043d2f2f0e8f8
sandra kinoti <sandra.kinoti@student.moringaschool.com>
2022-04-24 16:25:53 +0300

    add method that verifies user is on the user list

--- passlock.py
+++ passlock.py
@@ -36,3 +36,8 @@ class User:
         """
         method to verify whether the user is in our user_list or not
         """
+         a_user = ""
+        for user in User.user_list:
+            if(user.username == username and user.password == password):
+                    a_user == user.username
+        return a_user
